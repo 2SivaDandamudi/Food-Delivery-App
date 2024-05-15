c1ec91327ba6f19e60a2d15dc4046ef055ba15b4
2SivaDandamudi <tosivadandamudi@gmail.com>
2024-05-15 23:56:53 +0530

    Siva Dandamudi : Updated search functionality

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,7 +4,7 @@ import resList from "../../utils/mockData";
 
 const Body = () => {
     const [data, setData] = useState(resList)
-    const [isFilter, setIsFilter] = useState([...resList])
+    const [searchText, setSearchText] = useState(" ");
 
     const handleTopRated = () => {
         const topRatedList = data.filter((res)=> res.info.avgRatingString > 4)
@@ -15,6 +15,16 @@ const Body = () => {
 		setData(resList)
 	}
 
+    const handleInput = (e) => {
+        setSearchText(e.target.value)
+        setData(resList)
+    }
+
+const handleSearch = (e) => {
+    const filteredList = data.filter((res) => res.info.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+     setData(filteredList)
+}
+
     return(
         <div className="body">
             <div className="filter">
@@ -23,8 +33,8 @@ const Body = () => {
             <button className="clear-btn" onClick={clearSelection}>Clear Filter</button>
             </div>
             <div className="search">
-                <input className="search-field" placeholder="Search Item" ></input>
-                <button className="search-btn">search</button>
+                <input type="text" className="search-field" placeholder="Search Item" value={searchText} onChange={handleInput} ></input>
+                <button className="search-btn" onClick={handleSearch}>search</button>
             </div>
             </div>
             <div className="res-container">
