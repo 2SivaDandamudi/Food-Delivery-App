a4d17f84bf4c81e9422a57c36e1c8facf84d95a2
2SivaDandamudi <tosivadandamudi@gmail.com>
2024-05-22 17:07:20 +0530

    added few more useStates and setUseStates to class function

--- src/components/userClass.jsx
+++ src/components/userClass.jsx
@@ -5,7 +5,9 @@ class UserClass extends React.Component{
         super(props)
 
         this.state = {
-            count: 1
+            count: 1,
+            count2: 2,
+            count3: 3
         }
     }
 
@@ -13,15 +15,24 @@ class UserClass extends React.Component{
         return (
             <div className='user-card'>
                 <h2> Count: {this.state.count}</h2>
+                <h2> Count2: {this.state.count2}</h2>
+                <h2> Count3: {this.state.count3}</h2>
                 <button onClick={() => {
                     this.setState(
                         {
-                            count: this.state.count + 1
+                            count: this.state.count + 1,
+                            count2: this.state.count2 + 2
                         }
                     )
                 }}>
-                    Increase By 1
+                    Increase By 1 & 2
                     </button>
+                    <button onClick={() => {
+                        this.setState({
+                            count3: this.state.count3 + 3
+                        })
+                    }}>Increase by 3</button>
+                    
                 <h3> Location: {this.props.country} </h3>
                 <h3> Contact: @2Siva</h3>
             </div>
